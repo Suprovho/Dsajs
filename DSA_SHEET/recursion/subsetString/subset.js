cb9d0ba66acdddae0d3282af56fd4e1813629b49
Suprovho <106928567+Suprovho@users.noreply.github.com>
2025-02-04 22:10:31 +0530

    subset

---
+++ DSA_SHEET/recursion/subsetString/subset.js
@@ -0,0 +1,38 @@
+//     ""
+//    /   \
+//    "a"   ""
+//    /  \   /  \
+//    "ab"  "a" "b"  ""
+//    /  \   / \  / \  / \
+//    "abc" "ab" "ac" "a" "bc" "b" "c" ""
+
+function subset(s, list, current) {
+  if (s == "") {
+    list.push(current);
+    return list;
+  }
+
+  let char = s[0];
+  subset(s.substring(1), list, char + current); //include
+  subset(s.substring(1), list, current); //exclude
+}
+
+const list = [];
+subset("abc", list, "");
+console.log(list);
+
+function subsetNo(arr, current, list) {
+  if (arr.length === 0) {
+    list.push([...current]);
+    return list;
+  }
+
+  let ele = arr[0];
+  subsetNo(arr.slice(1), [...current, ele], list);
+  subsetNo(arr.slice(1), current, list);
+}
+
+const list2 = [];
+const arr = [1, 2, 3];
+subsetNo(arr, [], list2);
+console.log(list2);
