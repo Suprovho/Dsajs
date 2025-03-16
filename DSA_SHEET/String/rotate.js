b764c3202c73b959a6c3c533bd29d56fb7940932
Suprovho <106928567+Suprovho@users.noreply.github.com>
2025-03-17 00:09:51 +0530

    ""

--- DSA_SHEET/String/rotate.js
+++ DSA_SHEET/String/rotate.js
@@ -1,3 +1,5 @@
+// tricky question --
+// if we concat s+s then it will contain all the possible left shift ..
 var rotateString = function (s, goal) {
   if (s.length !== goal.length) {
     return false;
