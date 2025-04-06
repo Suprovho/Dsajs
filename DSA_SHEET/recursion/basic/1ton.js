7620b1d00556ca9e8cebdb0ea9d9596917438d20
Suprovho <106928567+Suprovho@users.noreply.github.com>
2025-04-06 20:48:31 +0530

    ""

--- DSA_SHEET/recursion/basic/1ton.js
+++ DSA_SHEET/recursion/basic/1ton.js
@@ -1,19 +1,20 @@
 function printNos(n) {
-     if (n==0) {
-        return;
-     }
-     printNos(n-1);
-     console.log(n);
+  if (n == 0) {
+    return;
+  }
+  printNos(n - 1);
+  console.log(n);
 }
 
 // printNos(10);
 
 function print1toN(n) {
-   if (n==0) {
-      return;
-   }
-   console.log(n);
-   print1toN(n-1);
+  if (n == 0) {
+    return;
+  }
+  console.log(n);
+  print1toN(n - 1);
 }
 
 console.log(print1toN(10));
+
--- DSA_SHEET/recursion/basic/factorial.js
+++ DSA_SHEET/recursion/basic/factorial.js
@@ -1,8 +1,11 @@
 function factorial(n) {
-    if (n<=1) {
-        return n;
-    }
-    return n*factorial(n-1)
+  if (n <= 1) {
+    return n;
+  }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(5));
+
+// in this type of recursion its like hey factorial(5) asking fac..(4) do u have ans and each one is passsing and waiting for next until it hit the base case then remaining one perform basis on that;
+
