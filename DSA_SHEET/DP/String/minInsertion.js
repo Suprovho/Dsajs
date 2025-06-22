56ac103d841723c945298304b0d997977d9d9c1d
Suprovho <106928567+Suprovho@users.noreply.github.com>
2025-06-23 01:15:15 +0530

    dp-23

--- DSA_SHEET/DP/String/minInsertion.js
+++ DSA_SHEET/DP/String/minInsertion.js
@@ -1,6 +1,8 @@
-var longestPalindromeSubseq = function(s) {
-    let t=s.split("").reverse().join("");
-    return lcs(s,t);
+var minInsertions = function (s) {
+  let n = s.length;
+  let t = s.split("").reverse().join("");
+  // The minimum insertions required is equal to the length of the string minus the length of its Longest Palindromic Subsequence
+  return n - lcs(s, t);
 };
 
 function lcs(s1, s2) {
@@ -15,7 +17,7 @@ function lcs(s1, s2) {
   }
   for (let ind1 = 1; ind1 <= n; ind1++) {
     for (let ind2 = 1; ind2 <= m; ind2++) {
-      if (s1[ind1 - 1] === s2[ind2 - 1]) {
+      if (s1[ind1 - 1] == s2[ind2 - 1]) {
         dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
       } else {
         dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
