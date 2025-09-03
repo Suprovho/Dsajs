15ed02e886452c6d0813d7c96943648551eca277
Suprovho <106928567+Suprovho@users.noreply.github.com>
2025-09-03 21:06:52 +0530

    ""

--- DSA_SHEET/Arrays/slidingWin-poin.js/minWin.js
+++ DSA_SHEET/Arrays/slidingWin-poin.js/minWin.js
@@ -1,46 +1,38 @@
+
 var minWindow = function (s, t) {
-  let m = s.length,
-    n = t.length;
-  if (m < n) return "";
+    let n = s.length;
+    let m = t.length;
+    if (n < m) return "";
 
-  let hashMap = new Map(); // Frequency map for characters in t
-  for (let i = 0; i < n; i++) {
-    hashMap.set(t[i], (hashMap.get(t[i]) || 0) + 1);
-  }
-  let l = 0,
-    r = 0;
-  let ct = 0; // Count of matching characters
-  let minLen = Infinity; // Initialize minLen to a large value
-  let sIndex = -1;
-
-  while (r < m) {
-    let rightChar = s[r];
-    if (hashMap.has(rightChar)) {
-      if (hashMap.get(rightChar) > 0) {
-        ct++;
-      }
-      hashMap.set(rightChar, hashMap.get(rightChar) - 1);
+    let mp = new Map();
+    for (let ch of t) {
+        mp.set(ch, (mp.get(ch) || 0) + 1);
     }
+    let count = mp.size;
 
-    while (ct === n) {
-      // When we have a valid window, try to minimize it
-      if (r - l + 1 < minLen) {
-        minLen = r - l + 1;
-        sIndex = l;
-      }
+    let start = 0;
+    let min_len = Infinity;
+    let left = 0; // left pointer
 
-      let leftChar = s[l];
-      if (hashMap.has(leftChar)) {
-        hashMap.set(leftChar, hashMap.get(leftChar) + 1);
-        if (hashMap.get(leftChar) > 0) {
-          ct--;
+    for (let right = 0; right < n; right++) {
+        if (mp.has(s[right])) {
+            mp.set(s[right], mp.get(s[right]) - 1);
+            if (mp.get(s[right]) === 0) count--;
         }
-      }
-      l++;
+
+        while (count === 0) {
+            if (right - left + 1 < min_len) { // one possible ans found
+                min_len = right - left + 1; 
+                start = left;
+            }
+
+            if (mp.has(s[left])) {
+                mp.set(s[left], mp.get(s[left]) + 1);
+                if (mp.get(s[left]) === 1) count++;
+            }
+            left++;
+        }
     }
 
-    r++;
-  }
-
-  return sIndex === -1 ? "" : s.substring(sIndex, sIndex + minLen);
+    return min_len === Infinity ? "" : s.substring(start, start + min_len);
 };
