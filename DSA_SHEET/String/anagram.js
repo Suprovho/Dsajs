320e6769c34750fc6c557f4af2201983bb22b524
Suprovho <suprovho25@gmail.com>
2025-11-06 01:01:36 +0530

    ""

--- DSA_SHEET/String/anagram.js
+++ DSA_SHEET/String/anagram.js
@@ -1,9 +1,20 @@
 var isAnagram = function (s, t) {
-  if (s.length !== t.length) return false;
-  if (s.length !== t.length) return false;
-  let temp=[...t];
-  let temp2=[...s];
-  t = temp.sort().join("");
-  s = temp2.sort().join("");
-  return s===t;
+    if (s.length !== t.length) return false;
+    let map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (!map.has(t[i])) return false;
+        else {
+            if (map.get(t[i]) <= 0) return false;
+            else map.set(t[i], (map.get(t[i])) - 1);
+        }
+    }
+
+    return true;
 };
+
+// return s.split("").sort().join("")===t.split("").sort().join("");
